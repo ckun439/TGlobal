19ad441ab68a2161213f087d10e7d64f926bb2b1
caitlin kung <ckun439@aucklanduni.ac.nz>
2020-06-02 21:38:34 +1200

    changes in interactivity

--- js/project.js
+++ js/project.js
@@ -1,13 +1,8 @@
-
-function nameEnter() {
-  $("thank").text("Thank you for your submission, we will get back to you shortly");
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML === " ") {
+    x.innerHTML = "Think global takes your application very seriously. We offer 30 places to join our exciting think global team for each trip therefore we have to be very selective. After filling out your email, please fill in the about me section. Add your name and a bit about yourself, why you deserve to become a part of the TG team and what you will bring to the table! Make sure to include your name! Good luck youngsters!";
+  } else {
+    x.innerHTML = " ";
+  }
 }
-
-$("#submitbutton").on("click",nameEnter);
-
-
-function displayName(){
-  $("#myName").text("Jose");
-}
-
-$("myButton").on("click",displayName)
